1491045fccef3780601b9880f330603b3fa7100d
selove-jara <selove.jara@gmail.com>
2024-10-09 10:01:22 +0400

    4. Категории в TODO List [#331991 #516551]

--- src/main/java/ru/job4j/todo/service/CategoryService.java
+++ src/main/java/ru/job4j/todo/service/CategoryService.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CategoryService {
-    Optional<ru.job4j.todo.model.Category> findById(int id);
+    Optional<Category> findById(int id);
 
-    List<ru.job4j.todo.model.Category> findAllOrderById();
+    List<Category> findAllOrderById();
 
     List<Category> findByListOfId(List<Integer> categoriesId);
 }
